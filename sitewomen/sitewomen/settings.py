0934393d17f0193705382347ab21e3c1d3e9901e
Gwynger <gwyngers@gmail.com>
2024-01-17 21:28:16 +0700

    Add password recovery

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -139,3 +139,6 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'users.authentication.EmailAuthBackend',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
--- sitewomen/users/forms.py
+++ sitewomen/users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 
 
 class LoginUserForm(AuthenticationForm):
@@ -54,4 +54,10 @@ class ProfileUserForm(forms.ModelForm):
             'last_name': forms.TextInput(attrs={'class': 'form-input'}),
         }
 
+
+class UserPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(label='Old password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    new_password1 = forms.CharField(label='New password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    new_password2 = forms.CharField(label='Password confirmation',
+                                    widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
