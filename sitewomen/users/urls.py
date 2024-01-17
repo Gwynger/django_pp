6eb659728d36fd77c72c9d22a3650645e3ffcf65
Gwynger <gwyngers@gmail.com>
2024-01-17 15:45:10 +0700

    Add register users

--- sitewomen/users/urls.py
+++ sitewomen/users/urls.py
@@ -7,6 +7,7 @@ app_name = 'users'
 urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
 
 
