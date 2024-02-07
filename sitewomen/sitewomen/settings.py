ff7220dc9d6812ca6b9fb89e5ea96d3c607f36a2
gwynger <gwyngers@gmail.com>
2024-02-07 20:23:59 +0700

    Update Dockerfile

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = env('DJ_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 INTERNAL_IPS = ['127.0.0.1']
 
 
